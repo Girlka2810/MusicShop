4ef33c955587744cd61eed542f3ec3b56c1af581
Kirill Kireev <kereeevkirill@gmail.com>
2021-02-28 20:13:03 +0300

    Add all buttons

--- app/src/main/java/com/example/musicshop/MainActivity.kt
+++ app/src/main/java/com/example/musicshop/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.musicshop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
